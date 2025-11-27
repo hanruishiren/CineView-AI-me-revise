79060ed5b6029c37c1c2efcdd0c1fbd8b8c49f06
hanruishiren <hanruishiren@163.com>
2025-11-28 01:12:54 +0800

    fix: 修复 vite.config.ts 中的编码问题

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) =\u003e {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: process.env.NODE_ENV === 'production' ? '/CineView-AI-me-revise/' : '/',
